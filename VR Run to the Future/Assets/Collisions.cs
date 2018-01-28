316499c54d1d11ec13969494163366842900fc20
Sterling Finn <sefinn@edu.uwaterloo.ca>
2018-01-28 04:59:24 -0500

    wasd implemented in n! runtime

---
+++ VR Run to the Future/Assets/Collisions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Collisions : MonoBehaviour {
+
+    public playerMover movement;
+    
+    
+    void OnCollisionEnter(Collision collisionInfo) {
+        if(collisionInfo.collider.tag == "Wall")
+        {
+            Debug.Log("hello");
+            movement.enabled = false;
+        }
+
+    }
+
+
+}
